51f41bc8ec0b9ba9223390d8ed7bead8c974834e
MaurizioCr <crispinomaurizio034@gmail.com>
2024-01-26 16:28:18 +0100

    Fixes

--- src/main/java/w7d5/mauriziocrispino/DTO/EventDTO.java
+++ src/main/java/w7d5/mauriziocrispino/DTO/EventDTO.java
@@ -5,5 +5,21 @@ import lombok.Getter;
 import java.util.Date;
 
 @Getter
-public record EventDTO(String title, String description, Date date, String location, int availableSeats) {
+public class EventDTO {
+    private String title;
+    private String description;
+    private Date date;
+    private String location;
+    private int availableSeats;
+
+    public EventDTO(String title, String description, Date date, String location, int availableSeats) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.availableSeats = availableSeats;
+    }
+
+    public EventDTO() {
+    }
 }
--- src/main/java/w7d5/mauriziocrispino/Repositories/AuthRepository.java
+++
@@ -1,8 +0,0 @@
-package w7d5.mauriziocrispino.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthRepository extends JpaRepository {
-}
